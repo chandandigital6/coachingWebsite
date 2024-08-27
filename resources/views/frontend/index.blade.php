a2b189a8f74e2c08eb2065baabe61c9bd1f53c40
mayank07feb <makedy07feb@gmail.com>
2024-08-27 13:28:37 +0530

    index page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,18 +6,22 @@
         <div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel" data-aos="fade-up">
             <ol class="carousel-indicators" data-aos="fade-down">
                 @foreach ($banners as $key => $banner)
-                    <li data-target="#header-carousel" data-slide-to="{{ $key }}" class="{{ $key == 0 ? 'active' : '' }}"></li>
+                    <li data-target="#header-carousel" data-slide-to="{{ $key }}"
+                        class="{{ $key == 0 ? 'active' : '' }}"></li>
                 @endforeach
             </ol>
             <div class="carousel-inner">
                 @foreach ($banners as $key => $banner)
-                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" style="min-height: 300px;" data-aos="fade-right">
-                        <img class="position-relative w-100" src="{{ asset('storage/' . $banner->image) }}" style="min-height: 300px; object-fit: cover;">
+                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" style="min-height: 300px;"
+                        data-aos="fade-right">
+                        <img class="position-relative w-100" src="{{ asset('storage/' . $banner->image) }}"
+                            style="min-height: 300px; object-fit: cover;">
                         <div class="carousel-caption d-flex align-items-center justify-content-center">
                             <div class="p-5" style="width: 100%; max-width: 900px;">
                                 <h5 class="text-white text-uppercase mb-md-3">{{ $banner->title }}</h5>
                                 <h1 class="display-3 text-white mb-md-4">{{ $banner->sub_title }}</h1>
-                                <a href="" class="btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2">Learn More</a>
+                                <a href="" class="btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2">Learn
+                                    More</a>
                             </div>
                         </div>
                     </div>
@@ -36,16 +40,18 @@
     <!-- Carousel End -->
 
     <!-- About Start -->
-    <div class="container-fluid py-5">
+    <div class="container-fluid">
         <div class="container py-5">
             @foreach ($abouts as $about)
                 <div class="row align-items-center">
                     <div class="col-lg-5" data-aos="fade-right">
-                        <img class="img-fluid rounded mb-4 mb-lg-0" src="{{ asset('storage/' . $about->image) }}" alt="">
+                        <img class="img-fluid rounded mb-4 mb-lg-0" src="{{ asset('storage/' . $about->image) }}"
+                            alt="">
                     </div>
                     <div class="col-lg-7" data-aos="fade-left">
                         <div class="text-left mb-4">
-                            <h5 class="text-primary text-uppercase mb-3" style="letter-spacing: 5px;">{{ $about->title }}</h5>
+                            <h5 class="text-primary text-uppercase mb-3" style="letter-spacing: 5px;">{{ $about->title }}
+                            </h5>
                             <h1>{{ $about->heading }}</h1>
                         </div>
                         <p>{!! $about->description !!}</p>
@@ -59,7 +65,7 @@
 
     <!-- Category Start -->
     <div class="container-fluid">
-        <div class="container">
+        <div class="container py-5">
             <div class="text-center mb-5" data-aos="fade-up">
                 <h5 class="text-primary text-uppercase mb-3" style="letter-spacing: 5px;">Subjects</h5>
                 <h1>Explore Top Subjects</h1>
@@ -68,7 +74,8 @@
                 @foreach ($services as $service)
                     <div class="col-lg-3 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="{{ $loop->index * 100 }}">
                         <div class="cat-item position-relative overflow-hidden rounded mb-2">
-                            <img class="img-fluid" src="{{ asset('storage/'.$service->image) }}" alt="{{ $service->title }}">
+                            <img class="img-fluid" src="{{ asset('storage/' . $service->image) }}"
+                                alt="{{ $service->title }}">
                             <a class="cat-overlay text-white text-decoration-none" href="">
                                 <h4 class="text-white font-weight-medium">{{ $service->title }}</h4>
                                 <span>{{ $service->heading }}</span>
@@ -83,26 +90,32 @@
 
     <!-- Courses Start -->
     <div class="container-fluid">
-        <div class="container">
+        <div class="container py-5">
             <div class="text-center mb-5" data-aos="fade-up">
                 <h5 class="text-primary text-uppercase mb-3" style="letter-spacing: 5px;">Courses</h5>
                 <h1>Our Popular Courses</h1>
             </div>
             <div class="row">
-                @foreach($course as $courses)
+                @foreach ($course as $courses)
                     <div class="col-lg-4 col-md-6 mb-4" data-aos="fade-up" data-aos-delay="100">
                         <div class="rounded overflow-hidden mb-2">
-                            <img class="img-fluid" src="{{ asset('storage/'.$courses->image) }}" alt="">
+                            <a href="{{ route('courseDetails-courses', $courses->id) }}">
+                                <img class="img-fluid" src="{{ asset('storage/' . $courses->image) }}" alt="">
+                            </a>
                             <div class="bg-secondary p-4">
                                 <div class="d-flex justify-content-between mb-3">
-                                    <small class="m-0"><i class="fa fa-users text-primary mr-2"></i>{{$courses->number_of_student}}</small>
-                                    <small class="m-0"><i class="far fa-clock text-primary mr-2"></i>{{$courses->duration}}</small>
+                                    <small class="m-0"><i
+                                            class="fa fa-users text-primary mr-2"></i>{{ $courses->number_of_student }}</small>
+                                    <small class="m-0"><i
+                                            class="far fa-clock text-primary mr-2"></i>{{ $courses->duration }}</small>
                                 </div>
-                                <a class="h5" href="{{route('courseDetails-courses',$courses->id)}}">{{$courses->title}}</a>
+                                <a class="h5"
+                                    href="{{ route('courseDetails-courses', $courses->id) }}">{{ $courses->title }}</a>
                                 <div class="border-top mt-4 pt-4">
                                     <div class="d-flex justify-content-between">
-                                        <h6 class="m-0"><i class="fa fa-star text-primary mr-2"></i>{{$courses->rating}}</h6>
-                                        <h5 class="m-0">₹{{$courses->price}}</h5>
+                                        <h6 class="m-0"><i
+                                                class="fa fa-star text-primary mr-2"></i>{{ $courses->rating }}</h6>
+                                        <h5 class="m-0">₹{{ $courses->price }}</h5>
                                     </div>
                                 </div>
                             </div>
@@ -110,13 +123,19 @@
                     </div>
                 @endforeach
             </div>
+            <!-- View All Button -->
+            <div class="text-center mt-4">
+                <a href="{{ route('courseDetails-courses', $courses->id) }}" class="btn btn-primary px-4 py-2">View All
+                    Courses</a>
+            </div>
         </div>
     </div>
     <!-- Courses End -->
 
+
     <!-- Testimonial Start -->
     <div class="container-fluid" data-aos="fade-up">
-        <div class="container">
+        <div class="container py-5">
             <div class="text-center mb-5" data-aos="fade-down">
                 <h5 class="text-primary text-uppercase mb-3" style="letter-spacing: 5px;">Testimonial</h5>
                 <h1>What Say Our Students</h1>
@@ -135,7 +154,7 @@
                                     {!! $testimonial->msg !!}
                                 </h4>
                                 <img class="img-fluid mx-auto mb-3" src="{{ asset('storage/' . $testimonial->image) }}"
-                                     alt="">
+                                    alt="">
                                 <h5 class="m-0">{{ $testimonial->name }}</h5>
                                 <span>Profession</span>
                             </div>
@@ -148,8 +167,8 @@
     </div>
     <!-- Testimonial End -->
 
-     <!-- Registration Start -->
-     <div class="container-fluid bg-registration" style="margin: 90px 0;" data-aos="fade-up">
+    <!-- Registration Start -->
+    <div class="container-fluid bg-registration" style="margin: 90px 0;" data-aos="fade-up">
         <div class="container py-5">
             <div class="row align-items-center">
                 <div class="col-lg-7 mb-5 mb-lg-0" data-aos="fade-right">
@@ -194,8 +213,8 @@
                                 </div>
                                 <div class="form-group">
                                     <label for="course" class="font-weight-bold text-white">Select Course</label>
-                                    <select class="form-control rounded-pill border-primary" name="course" id="course"
-                                        required>
+                                    <select class="form-control rounded-pill border-primary" name="course"
+                                        id="course" required>
                                         <option value="" disabled selected>Select Course</option>
                                         <option value="php">PHP</option>
                                         <option value="web designing">WEB DESIGNING</option>
@@ -223,25 +242,26 @@
                 <h1>Meet Our Teachers</h1>
             </div>
             <div class="row">
-                @foreach($teams as $team)
-                    <div class="col-md-6 col-lg-3 text-center team mb-4" data-aos="fade-up" data-aos-delay="{{ $loop->index * 100 }}">
+                @foreach ($teams as $team)
+                    <div class="col-md-6 col-lg-3 text-center team mb-4" data-aos="fade-up"
+                        data-aos-delay="{{ $loop->index * 100 }}">
 
 
                         <div class="team-item rounded overflow-hidden mb-2">
                             <div class="team-img position-relative">
-                                <img class="img-fluid" src="{{ asset('storage/'.$team->image) }}" alt="">
+                                <img class="img-fluid" src="{{ asset('storage/' . $team->image) }}" alt="">
                                 <div class="team-social">
-                                    <a class="btn btn-outline-light btn-square mx-1" href="{{$team->in_url}}"><i
+                                    <a class="btn btn-outline-light btn-square mx-1" href="{{ $team->in_url }}"><i
                                             class="fab fa-instagram"></i></a>
-                                    <a class="btn btn-outline-light btn-square mx-1" href="{{$team->fb_url}}"><i
+                                    <a class="btn btn-outline-light btn-square mx-1" href="{{ $team->fb_url }}"><i
                                             class="fab fa-facebook-f"></i></a>
-                                    <a class="btn btn-outline-light btn-square mx-1" href="{{$team->wat_url}}"><i
+                                    <a class="btn btn-outline-light btn-square mx-1" href="{{ $team->wat_url }}"><i
                                             class="fab fa-whatsapp"></i></a>
                                 </div>
                             </div>
                             <div class="bg-secondary p-4">
-                                <h5>{{$team->name}}</h5>
-                                <p class="m-0">{{$team->title}}</p>
+                                <h5>{{ $team->name }}</h5>
+                                <p class="m-0">{{ $team->title }}</p>
                             </div>
                         </div>
 
@@ -266,17 +286,18 @@
                         <div class="blog-item position-relative overflow-hidden rounded mb-2">
                             <img class="img-fluid" src="{{ asset('storage/' . $blog->image) }}" alt="">
                             <a class="blog-overlay text-decoration-none"
-                               href="{{ route('blogDetails-blog', $blog->id) }}">
-                                <h5 class="text-white mb-3">
-                                    {{ $blog->title }}
-                                </h5>
+                                href="{{ route('blogDetails-blog', $blog->id) }}">
+                                <h5 class="text-white mb-3">{{ $blog->title }}</h5>
                                 <p class="text-primary m-0">
                                     {{ \Carbon\Carbon::parse($blog->created_at)->format('F d, Y') }}</p>
-
                             </a>
                         </div>
                     </div>
                 @endforeach
+            </div>
+            <!-- View All Blogs Button -->
+            <div class="text-center mt-4">
+                <a href="{{ route('blogDetails-blog', $blog->id) }}" class="btn btn-primary px-4 py-2">View All Blogs</a>
             </div>
         </div>
     </div>
